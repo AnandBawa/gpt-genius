e1eff7d0e2b65372646b6df70f85aafab259dab4
Anand Bawa <anand.bawa@yahoo.com>
2023-12-22 19:19:17 +0530

    added landing and setup Clerk for authentication

---
+++ app/(dashboard)/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "GPTGenius",
+  description:
+    "GPTGenius: Your AI language companion. Powered by OpenAI, it enhances your conversations, content creation, and more!",
+};
+
+const layout = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default layout;
---
+++ app/(dashboard)/tours/page.js
@@ -0,0 +1,5 @@
+const ToursPage = () => {
+  return <h2>ToursPage</h2>;
+};
+
+export default ToursPage;
